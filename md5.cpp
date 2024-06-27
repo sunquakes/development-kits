8e282d799bf8f19a4ddc2d6ba231eaf86c52f10f
Shing Rui <sunquakes@outlook.com>
2024-06-27 21:28:50 +0800

    Md5 and copy md5 text to clipboard.

--- md5.cpp
+++ md5.cpp
@@ -1,5 +1,10 @@
-#include "md5.h"
 #include "ui_md5.h"
+#include <md5.h>
+#include <QCryptographicHash>
+#include <QClipboard>
+#include <QMessageBox>
+#include <QTimer>
+#include <qtimer.h>
 
 md5::md5(QWidget *parent)
     : QWidget(parent)
@@ -16,3 +21,86 @@ md5::~md5()
 void md5::on_button_accepted()
 {
 }
+
+void md5::on_submit_button_clicked(QAbstractButton *button)
+{
+}
+
+void md5::on_submit_button_accepted()
+{
+    QString text = ui->source_text->toPlainText();
+    if (!text.isEmpty()) {
+        QByteArray qba = QCryptographicHash::hash(text.toLatin1(), QCryptographicHash::Md5);
+        QString textMd5 = qba.toHex();
+        ui->upper32_text->setText(textMd5.toUpper());
+        ui->lower32_text->setText(textMd5.toLower());
+        QString textMd5_16 = textMd5.mid(8, 16);
+        qInfo() << textMd5_16;
+        ui->upper16_text->setText(textMd5_16.toUpper());
+        ui->lower16_text->setText(textMd5_16.toLower());
+    }
+}
+
+
+void md5::on_submit_button_rejected()
+{
+    ui->source_text->setText("");
+    ui->upper32_text->setText("");
+    ui->lower32_text->setText("");
+    ui->upper16_text->setText("");
+    ui->lower16_text->setText("");
+}
+
+
+void md5::on_upper32_copy_clicked()
+{
+    QString text = ui->upper32_text->text();
+    if (!text.isEmpty()) {
+        QClipboard *clipboard = QGuiApplication::clipboard();
+        clipboard->setText(text);
+        alert("Copied");
+    }
+}
+
+void md5::alert(QString text)
+{
+    QMessageBox *mbox = new QMessageBox;
+    mbox->setWindowTitle(text);
+    mbox->setText(text);
+    mbox->show();
+    QTimer::singleShot(2000, mbox, SLOT(hide()));
+}
+
+
+void md5::on_lower32_copy_clicked()
+{
+    QString text = ui->lower32_text->text();
+    if (!text.isEmpty()) {
+        QClipboard *clipboard = QGuiApplication::clipboard();
+        clipboard->setText(text);
+        alert("Copied");
+    }
+}
+
+
+void md5::on_upper16_copy_clicked()
+{
+    QString text = ui->upper16_text->text();
+    if (!text.isEmpty()) {
+        QClipboard *clipboard = QGuiApplication::clipboard();
+        clipboard->setText(text);
+        alert("Copied");
+    }
+}
+
+
+void md5::on_lower16_copy_clicked()
+{
+    QString text = ui->lower16_text->text();
+    if (!text.isEmpty()) {
+        QClipboard *clipboard = QGuiApplication::clipboard();
+        clipboard->setText(text);
+        alert("Copied");
+    }
+}
+
--- md5.h
+++ md5.h
@@ -2,6 +2,7 @@
 #define MD5_H
 
 #include <QWidget>
+#include <qabstractbutton.h>
 
 namespace Ui {
 class md5;
@@ -14,11 +15,24 @@ class md5 : public QWidget
 public:
     explicit md5(QWidget *parent = nullptr);
     ~md5();
-
 private slots:
-    void on_buttonBox_accepted();
-
     void on_button_accepted();
+
+    void on_submit_button_clicked(QAbstractButton *button);
+
+    void on_submit_button_accepted();
+
+    void on_submit_button_rejected();
+
+    void on_upper32_copy_clicked();
+
+    void alert(QString text);
+
+    void on_lower32_copy_clicked();
+
+    void on_upper16_copy_clicked();
+
+    void on_lower16_copy_clicked();
 
 private:
     Ui::md5 *ui;
